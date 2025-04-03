2a63c417783c026c1af852eeef3ec80a840e9e8e
Geo_Pro_09 <astrobrandon2013@gmail.com>
2025-04-03 18:01:43 -0400

    ON STEP 18

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rb;
+
+    float jumpForce = 5.7f;
+    public bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, .15f);
+        Debug.DrawRay(transform.position, Vector3.down * .15f,Color.red);
+
+        if(Input.GetButtonDown("Jump")){
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
