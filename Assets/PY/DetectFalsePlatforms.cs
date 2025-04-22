b3abbfce36ade7964659990dca95fc91ab902ce2
Code Ninjas Etobicoke <codeninjasetobicoke@outlook.com>
2025-04-22 18:07:20 -0400

    Completed

--- Assets/PY/DetectFalsePlatforms.cs
+++ Assets/PY/DetectFalsePlatforms.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class DetectFalsePlatforms : MonoBehaviour
 {
+    public float distance = 0.15f;
+    bool hit;
 
     void Update()
     {
+        hit = Physics.Raycast(transform.position, transform.forward, distance, 1 << 8);
+        Debug.DrawRay(transform.position, transform.forward * distance, Color.red);
 
+        if (hit == false)
+        {
+            Debug.Log("All clear!");
+        }
+        else
+        {
+            Debug.LogWarning("Be careful!");
+        }
     }
 }
